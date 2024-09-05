aff45db305963cc85e7de6c87375a75595ca3aa9
Nguyễn Ngọc Bảo <115100990+NNBaro@users.noreply.github.com>
2024-09-05 15:14:35 +0700

    Mới

--- Project/Project/Form1.Designer.cs
+++ Project/Project/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -37,15 +38,25 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(39, 139);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1287, 52);
+            this.label1.Size = new System.Drawing.Size(1255, 51);
             this.label1.TabIndex = 0;
             this.label1.Text = "ĐỒ ÁN XÂY DỰNG PHẦM MỀM QUẢN LÝ CỦA HÀNG PIZZA";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(498, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 95);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1408, 386);
+            this.ClientSize = new System.Drawing.Size(1316, 396);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
