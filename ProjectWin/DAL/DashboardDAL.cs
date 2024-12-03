3910f96f48916d825653e090a63401cbe64c8c9e
Huỳnh Văn Tiên <huynhtien030903@gmail.com>
2024-12-04 01:11:19 +0700

    Dal dashboard

---
+++ ProjectWin/DAL/DashboardDAL.cs
@@ -0,0 +1,47 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DAL
+{
+    public class DashboardDal
+    {
+        private static DashboardDal instance;
+        public static DashboardDal Instance => instance ?? (instance = new DashboardDal());
+
+
+        private DashboardDal() { }
+
+        public Dictionary<string, object> GetDashboard()
+        {
+            var result = new Dictionary<string, object>();
+            var today = DateTime.Today;
+            var tomorrow = today.AddDays(1);
+            using (var ctx = new DbConfig())
+            {
+                var totalCus = ctx.users
+                    .Count(item => Constant.Active.Equals(item.status) && Constant.User.Equals(item.role));
+                var totalStaff = ctx.users
+                    .Count(item => Constant.Active.Equals(item.status) && Constant.Staff.Equals(item.role));
+                var totalProduct = ctx.products
+                    .Count(item => Constant.Active.Equals(item.status));
+                var revenue = ctx.orders
+                    .Where(item => "done".Equals(item.status))
+                    .Sum(item => item.total_price) ?? 0;
+                var revenueToday = ctx.orders
+                    .Where(item => "done".Equals(item.status) 
+                    && item.created_at >= today && item.created_at <= tomorrow)
+                    .Sum (item => item.total_price) ?? 0;
+
+                result.Add(Constant.TotalCus, totalCus);
+                result.Add(Constant.TotalStaff, totalStaff);
+                result.Add(Constant.TotalProduct, totalProduct);
+                result.Add(Constant.Revenue, revenue);
+                result.Add(Constant.RevenueToday, revenueToday);
+            }
+            return result;
+        }
+    }
+}
---
+++ ProjectWin/DAL/UserDAL.cs
@@ -0,0 +1,78 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UserDal
+    {
+        private static UserDal _instance;
+        private UserDal()
+        {
+        }
+        public static UserDal instance => _instance ?? (_instance = new UserDal());
+
+        public User GetByUsername(string username)
+        {
+            using (var ctx = new DbConfig())
+            {
+                return ctx.users.FirstOrDefault(item => item.email.Equals(username));
+            }
+        }
+
+        public List<User> GetListUser()
+        {
+            using (var ctx = new DbConfig())
+            {
+                return ctx.users
+                    .Where(item => !Constant.Admin.Equals(item.role) && Constant.Active.Equals(item.status))
+                    .ToList();
+            }
+        }
+
+        public bool Save(User u)
+        {
+            using (var ctx = new DbConfig())
+            {
+                var user = ctx.users.FirstOrDefault(item => item.user_id == u.user_id);
+                if (user != null)
+                {
+                    user.full_name = u.full_name;
+                    user.password = u.password;
+                    user.updated_at = DateTime.Now;
+                }
+                else
+                {
+                    u.status = Constant.Active;
+                    u.role = Constant.Staff;
+                    u.created_at = DateTime.Now;
+                    ctx.users.Add(u);
+                }
+                return ctx.SaveChanges() > 0;
+            }
+        }
+
+        public bool Remove(int id)
+        {
+            using (var ctx = new DbConfig())
+            {
+                var user = ctx.users.FirstOrDefault(item => item.user_id == id);
+                if (user != null)
+                {
+                    user.status = Constant.Inactive;
+                    user.updated_at = DateTime.Now;
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
