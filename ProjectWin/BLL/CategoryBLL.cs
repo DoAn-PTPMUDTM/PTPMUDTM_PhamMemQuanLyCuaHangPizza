9600d6e5d06cfd0755f693ae5b4b90d86598279e
quylontocon <152693769+quylontocon@users.noreply.github.com>
2024-12-04 01:34:37 +0700

    BLLcmtcatelogy

---
+++ ProjectWin/BLL/CategoryBLL.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL;
+using DTO;
+
+namespace BLL
+{
+    public class CategoryBll
+    {
+        private static CategoryBll _instance;
+        public static CategoryBll instance => _instance ?? (_instance = new CategoryBll());
+
+        private CategoryBll()
+        {
+        }
+
+
+        public List<Category> FindAll()
+        {
+            return CategoryDal.instance.GetListCategory();
+        }
+
+        private string Save(Category c)
+        {
+            return CategoryDal.instance.Save(c)
+                ? Constant.Success
+                : Constant.Fail;
+        }
+
+        public CommonMessage Save(string cateId, string cateName, string cateDes)
+        {
+            if (string.IsNullOrEmpty(cateName))
+            {
+                return CommonMessage.Fail("Tên loại sản phẩm không được để trống");
+            }
+
+            var category = new Category
+            {
+                category_name = cateName,
+                description = cateDes
+            };
+
+            if (!string.IsNullOrEmpty(cateId))
+            {
+                category.category_id = Convert.ToInt32(cateId);
+            }
+
+            return CategoryDal.instance.Save(category)
+                ? CommonMessage.Success("Lưu thành công")
+                : CommonMessage.Fail("Lưu thất bại");
+        }
+
+        public CommonMessage Remove(string cateId)
+        {
+            if (string.IsNullOrEmpty(cateId))
+            {
+                return CommonMessage.Fail("Mã loại sản phẩm không được để trống");
+            }
+
+            var category = CategoryDal.instance.GetById(Convert.ToInt32(cateId));
+            if (category == null)
+            {
+                return CommonMessage.Fail("Không tìm thấy loại sản phẩm");
+            }
+
+            return CategoryDal.instance.Remove(Convert.ToInt32(cateId))
+                ? CommonMessage.Success("Xóa thành công")
+                : CommonMessage.Fail("Xóa thất bại");
+        }
+    }
+}
---
+++ ProjectWin/BLL/CommentBLL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL;
+using DTO;
+
+namespace BLL
+{
+    public class CommentBll
+    {
+        private static CommentBll _instance;
+        public static CommentBll instance => _instance ?? (_instance = new CommentBll());
+
+        private CommentBll()
+        {
+        }
+
+        public List<comment> FindAll()
+        {
+            return CommentDal.instance.GetListComment();
+        }
+
+        public CommonMessage Remove(string commentId)
+        {
+            if (string.IsNullOrEmpty(commentId))
+            {
+                return CommonMessage.Fail("Mã bình luận không được để trống");
+            }
+
+            return CommentDal.instance.Remove(Convert.ToInt32(commentId))
+                ? CommonMessage.Success("Xóa thành công")
+                : CommonMessage.Fail("Xóa thất bại");
+        }
+    }
+}
